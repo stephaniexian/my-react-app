cc8d21f5228b06508867a8f0b3ab5bd2c63282f3
Stephanie Xian <s.m.xian@wustl.edu>
2024-11-13 15:00:24 -0600

    created a new variables named squares that creates an aary of 9 nulls which show the 9 squares

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -18,22 +18,23 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
