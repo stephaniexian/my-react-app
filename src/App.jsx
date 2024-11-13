58fe7b40df91f7ae79187091e6a54325f68bf40e
Stephanie Xian <s.m.xian@wustl.edu>
2024-11-13 15:20:35 -0600

    added a new function names game that will show a list of the past moves when each move is made

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,22 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
